81ffd414d3f1ea315a5c932ca3f7188cfb39f510
Franziska Rogg <Franzi@klinikums-MacBook-Pro.local>
2026-02-18 19:00:13 +0100

    ps2.1

--- assignment2/ps2.1.R
+++ assignment2/ps2.1.R
@@ -56,8 +56,9 @@ quantile(df$women_parl, c(0.1, 0.25, 0.5, 0.75, 0.9), na.rm=TRUE)
 
 
 #c)
-#The coefficient on women_parl indicates the predicted change in EPI score for each additional percentage point of women in parliament. To get the predicted difference between the 25th and 75th percentile, we can either multiply the coefficient by the IQR or use predict(). Both give the same result:
+#The coefficient on women_parl indicates the predicted change in EPI score for a one-unit increase, a one-percent increase, of women in parliament. 
 
+#To get the predicted difference between the 25th and 75th percentile, we can either multiply the coefficient by the IQR or use predict(). Both give the same result:
 #Option1: use predict & create new data frame
 pred = predict(model1, newdata = data.frame(women_parl = c(p25, p75))) 
 #predict does: 'according to my regression model, what epi score would i expect if women_parl equals this value
@@ -78,7 +79,7 @@ modelsummary(model2)
 tidy(model2)
 
 #b)
-#The coefficient on women_parl decreases substantially once gov_eff is included. This suggests that part of the bivariate association was driven by government effectiveness being correlated with both women in parliament and environmental performance (omitted variable bias).
+#The coefficient on women_parl decreases significantly once gov_eff is included. It might show that parts of the coefficient in the biavariate actually associated with the effect of government effectiveness. The gov_eff variable might be related to both women in parliament and environmental performance. 
   
 #1.5 Demonstrating OVB
 #Recall the OVB formula: β ̃1 = βˆ1 + βˆ2 · δ ̃
@@ -92,7 +93,7 @@ beta2_mult = tidy(model2) %>% filter(term == "gov_eff") %>% pull(estimate) #b2ha
 #b) Auxilliary Regression
 aux = lm(gov_eff ~ women_parl, data = df)
 delta = tidy(aux) %>% filter(term == "women_parl") %>% pull(estimate) #delta = coefficient of women_parl in this aux model
-#It measures the relationship between the omitted variable (gov_eff) and your main X (women_parl); If women_parl goes up by 1 point, how much does gov_eff tend to go up?
+#It measures the relationship between the omitted variable (gov_eff) and main X (women_parl); If women_parl goes up by 1 point, how much does gov_eff tend to go up?
 
 
 #c) Verify the OVB Formula: 
@@ -103,10 +104,9 @@ round(beta1_mult + beta2_mult * delta, 4)
 round(beta1_biva, 4)
 
 #If the formula is correct, those two numbers should match (approximately). In the solution, they match (after rounding), confirming the formula. It proves that the difference between the bivariate and multiple coefficient is exactly explained by omitted variable bias.
-#Both values match, confirming the OVB formula.
 
 #d) 
-#The bias is positive because gov_eff is positively correlated with both women_parl (δ ̃ > 0) and with epi (βˆ2 > 0). This inflated the bivariate estimate.
+#The bias is positive because gov_eff is positively correlated with both women_parl and with the epi-score. 
 
 
 
@@ -117,7 +117,7 @@ modelsummary(model2, output = "markdown")
 #b) Robust SEs:
 modelsummary(model2, vcov = "robust", output = "markdown")
   
-#c) SEs may differ somewhat but conclusions typically don’t change with this sample.
+#c) Conclusions won't be affected by this.  
   
   
 #1.7 Presenting Results
